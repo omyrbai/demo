65a0276bd823da98a6cbf0b36ec8c350dd3094ab
Omirbay <aydaxo@bk.ru>
2023-04-17 16:39:18 +0500

    new comment line added

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     #1 birinsh comment
-
+    #2 ekinshi komment
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
