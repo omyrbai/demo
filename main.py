dd92ce8d6a3b9eb5204dcee672e204523894c24e
ulzada89 <k.tyler@mail.ru>
2023-04-17 16:31:02 +0500

    addes comment

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    #1 birinshi comment
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
