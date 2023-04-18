d7be8760fcfabc206cacb4424bfdaa92b436b594
Omirbay <aydaxo@bk.ru>
2023-04-18 14:51:26 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

---
+++ gtranslate.py
@@ -0,0 +1,7 @@
+from googletrans import Translator
+name = 'asd'
+def gtranslate(word):
+    translator = Translator()
+    translator.translate('make')
+    print(googletrans.languages)
+#lib install pip install googletrans
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from googletrans import Translator
-translator = Translator()
-translator.translate('make')
-#lib install pip install googletrans
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'The random number is: {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 if __name__ == '__main__':
-    print_hi(name=number)
+    gtranslate(name)
