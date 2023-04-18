d2aba4b18419c3293563dddeae288ccec33cf1b7
Omirbay <aydaxo@bk.ru>
2023-04-18 15:11:14 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- gtranslate.py
+++ gtranslate.py
@@ -1,8 +1,1 @@
 import googletrans
-from googletrans import Translator
-#lib install pip install googletrans
-
-print(googletrans.LANGUAGES)
-translator = Translator()
-text = "this is a sample text"
-print(translator.detect(text))
