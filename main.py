93ec0fda5aba062da3817fa2c105bb427d1eb00e
Omirbay <aydaxo@bk.ru>
2023-04-18 14:09:40 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#comment No2 this is a remark made by Omir.
 
-
-
-#  this line contains a remark that I just made
-# this is a new line just for example.
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    #new comment deleted 3 comments
-    #new comment by Ulzada
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-
-# Hi there Omyrbai, I pulled your repo and changed the main file ))
-
-#koreik bilmedim ekewmiz qaytip maseleni bolisip aliwdi da islep koriw kerek misali ulken project islesek
-#bolisip islewwge boladigo.
