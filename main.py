caec72b654546f31f22d098a72eaf21a27ff8a2f
Omirbay <aydaxo@bk.ru>
2023-04-18 14:44:33 +0500

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import random
-
-number = random.randint(0, 10)
-for x in range(number):
-    if x != 0:
-        print(x)
-
+from googletrans import Translator
+translator = Translator()
+translator.translate('make')
+#lib install pip install googletrans
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'The random number is: {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 if __name__ == '__main__':
     print_hi(name=number)
